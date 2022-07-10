6bb856a8e098f7149232798560649542b2c44ad1
josswe26 <josec.sanchez@icloud.com>
2022-07-10 08:27:46 +0000

    Add organisations views, templates and urls

--- organizations/views.py
+++ organizations/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Organization
 
-# Create your views here.
+
+def display_organizations(request):
+    """
+    A view to display the organizations available
+    for the user to support
+    """
+    organizations = Organization.objects.all()
+
+    template = 'organizations/organizations.html'
+
+    context = {
+        'organizations': organizations,
+    }
+
+    return render(request, template, context)
