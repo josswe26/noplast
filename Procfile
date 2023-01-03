1d3fa0ecd2c7c561bf3794ee20ccdacb32aa53ec
josswe26 <josec.sanchez@icloud.com>
2023-01-03 23:46:40 +0000

    Set up for deployment on Render

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn noplast.wsgi:application
--- noplast/settings.py
+++ noplast/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['noplast.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['localhost']
+
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
